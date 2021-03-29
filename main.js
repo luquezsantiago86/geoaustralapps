c1feb36aa84240b70586e85441df7af7434268a0
luquezsantiago86 <luquezsantiago86@gmail.com>
2021-03-29 15:42:42 +0100

    m

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
 $('#devBtn').on('click', function(e) {
     e.preventDefault();
     $('#devModal').modal('show').find('.modal-content').load($(this).attr('href'));
